f43820ee2a36c155b0521b687c0a5ee9b82db725
3wguap <rnbrell@yandex.by>
2026-03-26 01:10:17 +0400

    2,2-2,4

---
+++ src/main/scala/lectures/week1basics/Expressions.scala
@@ -0,0 +1,7 @@
+package lectures.week1basics
+
+object Expressions {
+  def main(args:Array[String]): Unit = {
+    
+  }
+}
--- src/main/scala/lectures/week1basics/StringOperations.scala
+++ src/main/scala/lectures/week1basics/StringOperations.scala
@@ -2,6 +2,11 @@ package lectures.week1basics
 
 object StringOperations {
   def main(args:Array[String]): Unit = {
-
+    val asString: String = "Scala course"
+    println(asString.length)
+    println(asString.charAt(11))
+    println(asString.reverse)
+    println(asString.substring(0,5))
+    println(asString.take(5))
   }
 }
